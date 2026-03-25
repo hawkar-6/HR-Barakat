5c1486186efb1b8915b0326e157e67b2cdb714f6
hawkar <engineerhawkar99@gmail.com>
2026-03-25 09:32:13 +0300

    modify login welcome signup main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
-import 'screen/welcome-page.dart'; // لێرەدا فایلەکە دەناسینێنین
+import 'package:barakat/screen/login_page.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: WelcomePage()),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Barakat App',
+      theme: ThemeData(useMaterial3: true),
+      home: const LoginPage(),
+    );
+  }
 }
